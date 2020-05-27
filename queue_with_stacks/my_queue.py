2bded4733e77721128c314dcba2720093dcffa75
dueka <ekanemd@live.com>
2020-05-27 10:03:33 +0100

    commit the queue

--- queue_with_stacks/my_queue.py
+++ queue_with_stacks/my_queue.py
@@ -4,7 +4,9 @@ class MyQueue(object):
         """
         Initialize your data structure here.
         """
-        
+        # stack representing queue
+        self.s1 = []
+        self.s2 = []
 
     def push(self, x):
         """
@@ -12,33 +14,51 @@ class MyQueue(object):
         :type x: int
         :rtype: None
         """
-        
+        # empty stack 1 into stack 2
+        while len(self.s1) != 0:
+            self.s2.append(self.s1.pop())
+
+        # push item into the first stack
+        self.s1.append(x)
+        while len(self.s2) != 0:
+            self.s1.append(self.s2.pop())
 
     def pop(self):
         """
         Removes the element from in front of queue and returns that element.
         :rtype: int
         """
-        
+        if len(self.s1) == 0:
+            print("Q is Empty")
+        x = self.s1[-1]
+        self.s1.pop()
+        return x
 
     def peek(self):
         """
         Get the front element.
         :rtype: int
         """
-        
+        return self.s1[-1]
 
     def empty(self):
         """
         Returns whether the queue is empty.
         :rtype: bool
         """
-        
+        return not bool(self.s1)
 
 
 # Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+obj = MyQueue()
+obj.push(1)
+obj.push(2)
+obj.push(3)
+
+param_2 = obj.pop()
+param_3 = obj.peek()
+param_4 = obj.empty()
+
+print(param_2)
+print(param_3)
+print(param_4)
